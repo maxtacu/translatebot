9efb30dbbbc3ee47705bdb71ca05da70d4701de4
Maxim Tacu <maxim.tacu@olx.com>
2020-03-13 17:44:12 +0000

    Added emoji cleaner and language detector to not translate from english

--- translatebot.py
+++ translatebot.py
@@ -1,7 +1,14 @@
 from googletrans import Translator
+from langdetect import detect
 import telebot
 import config
+import logging
+import emoji
+import re
 
+
+logging.basicConfig(filename='bot.log',level=logging.ERROR)
+logger = logging.getLogger(__name__)
 bot = telebot.TeleBot(config.telegram["token"])
 translator = Translator(service_urls=config.google["service_urls"])
 
@@ -12,26 +19,45 @@ def start(message):
 
 @bot.channel_post_handler(func=lambda message: True, content_types=["text"])
 def message(message):
-    bot.send_message(message.chat.id, translate(message.text))
+    try:
+        bot.send_message(message.chat.id, translate(message.text))
+    except:
+        logger.error("Fatal error in channel handler", exc_info=True)
 
 @bot.channel_post_handler(func=lambda message: True, content_types=["photo", "video"])
 def message(message):
-    bot.send_message(message.chat.id, translate(message.caption))
+    try:
+        bot.send_message(message.chat.id, translate(message.caption))
+    except:
+        logger.error("Fatal error in channel handler", exc_info=True)
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def message(message):
-    bot.send_message(message.chat.id, translate(message.text))
+    try:
+        bot.send_message(message.chat.id, translate(message.text))
+    except:
+        logger.error("Fatal error in message handler", exc_info=True)
 
 @bot.message_handler(func=lambda message: True, content_types=["photo", "video"])
 def message(message):
-    bot.send_message(message.chat.id, translate(message.caption))
+    try:
+        bot.send_message(message.chat.id, translate(message.caption))
+    except:
+        logger.error("Fatal error in message handler", exc_info=True)
+
+def remove_emoji(text):
+    result = emoji.demojize(text)
+    result = re.sub(':.*?:', '', result)
+    return result
 
 def translate(text):
-    try:
-        result = translator.translate(text, dest='en')
+    cleantext = remove_emoji(text)
+    if detect(cleantext) != 'en':
+        result = translator.translate(cleantext, dest='en')
         return result.text
-    except Exception as e:
-        print(e)
+    else:
+        return
+
 
 def main():
     bot.polling(timeout=30)
